c06cd08e034567df40f405e764913b3bab832db1
Andrew Grow <andrew.gahov@gmail.com>
2025-07-10 14:27:18 +0200

    install puppeteer-extra-plugin-stealth

--- src/tools/puppeteer.js
+++ src/tools/puppeteer.js
@@ -1,10 +1,16 @@
 // /src/tools/puppeteer.js
 
-import puppeteer from 'puppeteer';
-import path, {dirname} from 'path';
+// import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import path from 'path';
 import fs from 'fs';
 import { filePath } from "../utils/wrappers/filePath.js";
 import { v4 as UUIDv4 } from 'uuid';
+import { setTimeout } from "node:timers/promises";
+
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+puppeteer.use(StealthPlugin());
 
 // prepare cacheDir
 const cacheDir = filePath("../../../cache")
@@ -20,16 +26,20 @@ export async function makeWebsiteScreenshot(url) {
 
     const screenshotPath = path.join(cacheDir, `${uniqUUID}.png`);
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    // puppeteer usage as normal
+    await puppeteer.launch({ headless: true }).then(async browser => {
+        console.log('Running puppeteer..')
+        const page = await browser.newPage()
+        await page.goto('https://webhook.site/1e904a6a-d7c1-4bf8-b8d2-808892ac1758')
+        // await page.goto(url)
+        await setTimeout(3100);
+        await page.screenshot({
+            path: screenshotPath,
+            fullPage: true
+        });
+        await browser.close()
+        console.log(`All done, check the screenshot. ✨`)
+    })
 
-    // await page.goto(url, {
-    await page.goto("https://webhook.site/1e904a6a-d7c1-4bf8-b8d2-808892ac1758", {
-        waitUntil: 'networkidle2',
-    });
-    await page.screenshot({
-        path: screenshotPath,
-    });
-    await browser.close();
     return screenshotPath;
 }
